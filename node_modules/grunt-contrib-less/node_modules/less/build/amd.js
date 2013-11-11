2980b742c283961136823f364c5fc82042a7860d
Zak Greene <zak@zakgreene.com>
2013-11-11 01:43:47 -0500

    Revert "upgraded to Grunt 0.4 inadvertently, which changes everything, but at least now it works yay."
    This reverts commit faba544bf5322856e76ae1527d1936ea7715723a.

--- node_modules/grunt-contrib-less/node_modules/less/build/amd.js
+++ node_modules/grunt-contrib-less/node_modules/less/build/amd.js
@@ -2,5 +2,5 @@
 //
 // Define Less as an AMD module.
 if (typeof define === "function" && define.amd) {
-    define(function () { return less; } );
+    define("less", [], function () { return less; } );
 }
